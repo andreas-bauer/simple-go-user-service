67c6b506b6c76801b37f7b863b11040f8811c360
Andreas Bauer <dev@andreasbauer.org>
2019-01-09 20:45:50 +0100

    Use logrus instead of log

--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -4,12 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"log"
 	"net/http"
 	"strings"
-
-	"github.com/andreas-bauer/simple-go-user-service/pkg/model"
-	"github.com/gorilla/mux"
 )
 
 var users []model.User
@@ -21,7 +20,7 @@ func init() {
 }
 
 func GetUsers(writer http.ResponseWriter, req *http.Request) {
-	log.Println("GetUsers")
+	logrus.Info("Http Request: GET /users/")
 
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(users)
@@ -29,11 +28,11 @@ func GetUsers(writer http.ResponseWriter, req *http.Request) {
 
 func GetUser(writer http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
-	log.Println("GetUser")
+	logrus.Info("Http Request: GET /users/", params["Email"])
 
 	user, err := doGetUser(params["Email"])
 	if err != nil {
-		log.Println(err)
+		logrus.WithError(err)
 		http.Error(writer, err.Error(), http.StatusNotFound)
 		return
 	}
@@ -43,6 +42,7 @@ func GetUser(writer http.ResponseWriter, req *http.Request) {
 }
 
 func CreateUser(writer http.ResponseWriter, req *http.Request) {
+	logrus.Info("Http Request: POST /users/")
 	var user model.User
 	_ = json.NewDecoder(req.Body).Decode(&user)
 	user.Role = strings.ToUpper(user.Role)
@@ -50,7 +50,7 @@ func CreateUser(writer http.ResponseWriter, req *http.Request) {
 	_, err := doGetUser(user.Email)
 	if err == nil {
 		msg := fmt.Sprintf("User with email %v already exist!", user.Email)
-		log.Println(msg)
+		logrus.Error(msg)
 		http.Error(writer, msg, http.StatusConflict)
 		return
 	}
@@ -68,8 +68,9 @@ func CreateUser(writer http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(writer).Encode(user)
 }
 
-func DeleteUser(writer http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
+func DeleteUser(writer http.ResponseWriter, req *http.Request) {
+	logrus.Info("Http Request: DELETE /users/")
+	params := mux.Vars(req)
 	for index, item := range users {
 		if item.Email == params["Email"] {
 			users = append(users[:index], users[index+1:]...)
