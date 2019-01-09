aff06fe530dae3171b90fc18cb3e343824646501
Andreas Bauer <dev@andreasbauer.org>
2019-01-09 20:44:16 +0100

    Move HTTP server into own go file

--- cmd/user-service/main.go
+++ cmd/user-service/main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"github.com/andreas-bauer/simple-go-user-service/pkg/http/rest"
-	"github.com/andreas-bauer/simple-go-user-service/pkg/model"
-	"log"
-	"net/http"
-)
-
-var users []model.User
-
-const (
-	port = ":8080"
 )
 
 func main() {
-	router := rest.Router()
-
-	log.Println("Starting server on port " + port)
-	log.Fatal(http.ListenAndServe(port, router))
+	server := &rest.Instance{}
+	server.Start()
 }
---
+++ pkg/http/rest/server.go
@@ -0,0 +1,48 @@
+package rest
+
+import (
+	"context"
+	"github.com/Sirupsen/logrus"
+	"github.com/andreas-bauer/simple-go-user-service/pkg/mongo"
+	"net/http"
+	"time"
+)
+
+type Instance struct {
+	db         *mongo.DB
+	httpServer *http.Server
+}
+
+const (
+	port = ":8080"
+)
+
+func (srv *Instance) Start() {
+	// Startup DB
+	//srv.db = MustOpenDb(dataDir)
+	//defer srv.closeDb()
+
+	// Startup HTTP
+	logrus.Info("Http Server starting with address ", port)
+	srv.httpServer = &http.Server{Addr: port, Handler: Router()}
+	err := srv.httpServer.ListenAndServe()
+
+	if err != http.ErrServerClosed {
+		logrus.WithError(err).Error("Http Server stopped unexpected")
+		srv.Shutdown()
+	} else {
+		logrus.WithError(err).Info("Http Server stopped")
+	}
+}
+
+func (srv *Instance) Shutdown() {
+	if srv.httpServer != nil {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		err := srv.httpServer.Shutdown(ctx)
+		if err != nil {
+			logrus.WithError(err).Error("Failed to shutdown http server gracefully")
+		} else {
+			srv.httpServer = nil
+		}
+	}
+}
